d419087f2f979111e7a2b8f3f2b907354a4c0911
ntweb <nathanxp@hotmail.com>
2017-03-11 10:34:46 +0100

    Cookie bug fixed

--- public/js/cookie/cookie.js
+++ public/js/cookie/cookie.js
@@ -1,12 +1,12 @@
+var storage = Storages.localStorage;
+// storage.setExpires(7);
+var is_cookie_info_hidden = storage.isSet('is_cookie_info_hidden');
+
+
 $(document).ready(function(){
 
 	// library
 	// https://github.com/julien-maurel/js-storage
-
-	var storage = Storages.localStorage;
-	// storage.setExpires(7);
-
-	var is_cookie_info_hidden = storage.isSet('is_cookie_info_hidden');
 	if (is_cookie_info_hidden) 
 		$('#cookie-info').hide(0);
 	else
