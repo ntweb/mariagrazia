a0d3c8d5c93d7f1f9124f30ecf01c50bb0de22e8
ntweb <nathanxp@hotmail.com>
2017-03-19 09:59:08 +0100

    Privacy label fixed

--- resources/lang/it/web.php
+++ resources/lang/it/web.php
@@ -2,6 +2,6 @@
 
 return [
     'cookie' => 'Questo sito utilizza cookie proprietari e di terze parti al fine di migliorare i propri servizi.<br>Per ulteriori informazioni consulta la nostra <a href="'.page_url('cookie').'">Politica relativa ai cookie</a>.<br>Chiudendo questo banner, scorrendo questa pagina, cliccando su un link o proseguendo la navigazione in altra maniera, acconsenti all\'uso dei cookie.',
-    'privacy_check' => 'Inviado il form dichiaro di aver preso visione ed aver accettato <a href="'.page_url('cookie').'">l\'informativa alla privacy</a>',
+    'privacy_check' => 'Inviado il form dichiaro di aver preso visione ed aver accettato <a href="'.page_url('privacy').'">l\'informativa alla privacy</a>',
 ];
 
